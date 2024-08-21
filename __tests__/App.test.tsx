bd975d20f239485bdcfedb622f7a7e22da650253
Omer Quadri <v-oquadri@bayshore.ca>
2024-08-21 12:04:03 -0400

    Add default test

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,17 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// Note: import explicitly to use the types shipped with jest.
+import { it } from '@jest/globals';
+import { render } from '@testing-library/react-native';
+
+jest.useFakeTimers();
+
+it('renders correctly', () => {
+  render(<App />);
+});
