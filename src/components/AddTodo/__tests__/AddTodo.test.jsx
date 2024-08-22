ec9d4408acfbf28e1f44d7e04b4b543bf0d43697
Omer Quadri <v-oquadri@bayshore.ca>
2024-08-22 15:08:15 -0400

    Move tests

--- src/components/AddTodo/__tests__/AddTodo.test.jsx
+++ src/components/AddTodo/__tests__/AddTodo.test.jsx
@@ -7,6 +7,31 @@ jest.useFakeTimers();
 
 // !UNIT TEST
 
+/**
+ * 3. Basic example
+ */
+// Can also be it() or test()
+// Describe anatomy of a test
+//  •	Import Statements: Import necessary modules, components, and utilities.
+// 	•	Setup: This might include mock setup, initializing test data, or configuring certain global settings.
+// 	•	Test Suites (describe blocks): Group related tests together.
+// 	•	Individual Tests (it or test blocks): Define each test case.
+// 	•	Assertions: Validate the behavior of your component or function.
+
+describe('Math Utilities', () => {
+  // describe => test suite
+  it('should add two numbers correctly', () => {
+    // test case
+    const test = 2 + 2;
+    expect(test).toEqual(4); // assertion
+  });
+
+  it('should multiply two numbers correctly', () => {
+    const test = 12 * 12;
+    expect(test).toEqual(144); // assertion
+  });
+});
+
 describe('render AddTodo component', () => {
   it('should render input with placeholder', () => {
     const { getByPlaceholderText } = render(<AddTodo />);
